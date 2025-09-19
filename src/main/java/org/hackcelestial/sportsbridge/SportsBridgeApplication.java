afe7210d682b292f686c69c51c16840c55887899
SebastianBenjamin <benjaminsebastian156@gmail.com>
2025-09-19 11:23:40 +0530

    Version 1

---
+++ src/main/java/org/hackcelestial/sportsbridge/SportsBridgeApplication.java
@@ -0,0 +1,13 @@
+package org.hackcelestial.sportsbridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsBridgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportsBridgeApplication.class, args);
+    }
+
+}
